ec0303af5f7183dbf149aac2fef1960e84f7e6f6
dilip gurjar <gurjardilip264@gmail.com>
2026-09-08 11:06:21 +0530

    auth controller is updated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,13 +6,14 @@ import { LoginDto } from './dto/login.dto';
 @Controller('api') //auth
 export class AuthController {
 constructor(private readonly authService:AuthService){}
- 
+
+// this is main register method to register both user and admin
     @Post('register')
     register(@Body()registerDto:RegisterDto){
         return this.authService.register(registerDto)
     }
 
-
+// this is login method for user and admin
     @Post('login')
     login(@Body() loginDto:LoginDto){
   return this.authService.login(loginDto);
